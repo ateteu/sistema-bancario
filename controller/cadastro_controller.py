994043540c4241c64ab7ca06b8980b09de2eff06
Mateus <mr.lordblader@gmail.com>
2025-05-25 01:52:05 -0300

    Cria rascunho de controlador de cadastro

--- controller/cadastro_controller.py
+++ controller/cadastro_controller.py
@@ -2,3 +2,20 @@
 # Métodos:
 #     cadastrar_pessoa(dados: dict)
 #     (futuro) cadastrar_pessoa_juridica(dados: dict)
+
+from model.pessoa import Pessoa
+from model.pessoa_fisica import PessoaFisica
+from dao.pessoa_dao import PessoaDAO
+
+def cadastrar_pessoa(dados: dict):
+    try:
+        pessoa = PessoaFisica.from_dict(dados)
+        PessoaDAO.salvar(pessoa)
+        return {"status": "sucesso", "mensagem": "Conta criada com sucesso"}
+    
+    except ValueError as e:
+        return {"status": "erro", "mensagem": str(e)}
+
+    except Exception as e:
+        return {"status": "erro", "mensagem": "Erro inesperado ao cadastrar pessoa"}
+    
